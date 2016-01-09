30c0c4c48de6d0ddce2c08d96e0dc152dfedd190
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-01-09 13:21:35 -0500

    Initial commit

---
+++ src/main/java/wisdom/Quote.java
@@ -0,0 +1,57 @@
+package wisdom;
+
+import org.springframework.data.annotation.Id;
+import java.util.Date;
+
+public class Quote {
+
+	@Id private String id;
+
+	private String text;
+	private String author;
+	private boolean local = false;
+	private Date createdAt = new Date();
+	private Date activeAt = new Date();
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public boolean isLocal() {
+		return this.local;
+	}
+
+	public void setLocal(boolean active) {
+		this.local = local;
+	}
+
+	public void setCreatedAt(Date date) {  
+    		this.createdAt = (Date) date.clone();  
+	}  
+  
+	public Date getCreatedAt() {  
+    		return (Date) this.createdAt.clone();  
+	}
+
+	public void setActiveAt(Date date) {  
+    		this.activeAt = (Date) date.clone();  
+	}  
+  
+	public Date getActiveAt() {  
+    		return (Date) this.activeAt.clone();  
+	}
+
+	
+}
