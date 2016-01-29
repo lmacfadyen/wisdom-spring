f55ea4f4159a5fc12b7768642695c1e4d005e09c
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-01-29 18:15:57 -0500

    Config to expose id in JSON and build updates

--- src/main/java/wisdom/Quote.java
+++ src/main/java/wisdom/Quote.java
@@ -13,6 +13,14 @@ public class Quote {
 	private Date createdAt = new Date();
 	private Date activeAt = new Date();
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getText() {
 		return text;
 	}
---
+++ src/main/java/wisdom/SpringDataRestConfig.java
@@ -0,0 +1,30 @@
+package wisdom;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+
+/**
+ * Created by larrymacfadyen on 2016-01-29.
+ */
+@Configuration
+class SpringDataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+        return new RepositoryRestConfigurerAdapter() {
+            @Override
+            public void configureRepositoryRestConfiguration(
+                    RepositoryRestConfiguration config) {
+                config.exposeIdsFor(Quote.class);
+            }
+        };
+
+    }
+
+}
