7aaa312bce31d33923974b8936bd1cab92a678a6
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2016-12-30 17:05:46 -0500

    Updates from past and to increase page size default

--- src/main/java/wisdom/QuoteRepository.java
+++ src/main/java/wisdom/QuoteRepository.java
@@ -1,12 +1,11 @@
 package wisdom;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "quotes", path = "quotes")
+import java.util.List;
+
+//@RepositoryRestResource(collectionResourceRel = "quotes", path = "quotes")
 public interface QuoteRepository extends MongoRepository<Quote, String> {
 
 	List<Quote> findByAuthor(@Param("author") String author);
--- src/main/java/wisdom/SpringDataRestConfig.java
+++ src/main/java/wisdom/SpringDataRestConfig.java
@@ -1,11 +1,11 @@
 package wisdom;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+//import org.springframework.http.MediaType;
 
 
 /**
@@ -21,7 +21,11 @@ class SpringDataRestConfig {
             @Override
             public void configureRepositoryRestConfiguration(
                     RepositoryRestConfiguration config) {
+                // Expose the id attribute over the REST interface
                 config.exposeIdsFor(Quote.class);
+                // Specify JSON instead of default HAL+JSON
+                //config.setDefaultMediaType(MediaType.APPLICATION_JSON);
+		 config.setDefaultPageSize(300);
             }
         };
 
